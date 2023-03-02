fd90b3a94dacf538704825a3435bb9166d805fed
Pranay Baid <baid2@wisc.edu>
2023-03-02 08:48:30 -0600

    Step 6 done

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+if [[$# -ne 2]]; then
+    echo "usage: $0 <dir><n>"1>&2
+    exit 1
+fi
+if [[!-d "$1"]]; then
+    echo "error: $1 is not a directory" 1>&2
+    exit 1
+fi
+find "$1" -type f -size+"$2"c -delete
