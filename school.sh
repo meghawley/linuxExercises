546e08ca651ff01f4b9dee834475713947378123
kgupta42 <125522532+kgupta42@users.noreply.github.com>
2023-03-08 15:00:31 -0600

    Update school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | {
+  sum=0
+  count=0
+  while read value; do
+    sum=$((sum+value))
+    count=$((count+1))
+  done
+  average=$((sum/count))
+  echo "Average TotalAssessedValue for MADISON SCHOOLS district: $average"
+}
